a3172db791b89f8150c996899d2965f3237ce446
candy-hub <1340744230@qq.com>
2019-11-15 22:47:58 +0800

    修改——candy

--- src/main/java/com/video/dao/RecordRepository.java
+++ src/main/java/com/video/dao/RecordRepository.java
@@ -4,6 +4,10 @@ package com.video.dao;
 import com.video.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecordRepository extends JpaRepository<Record,Integer> {
+
+    List<Record> findAllByUserId(Integer userId);
 
 }
